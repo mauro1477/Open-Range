d506d1c25bd2abcab1273d461362a5626dad15a6
mauro <maurovargas7725@gmail.com>
2025-03-23 18:34:54 -0600

    Added footer component and styles content-wrapper

---
+++ wp-content/themes/timber-starter-theme/assets/js/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="footer">
+        <p> Copyright © 2025 Open Range. All Rights Reserved.</p>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+            }
+        },
+        methods: {
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .footer{
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #000;
+        color: white;
+        text-align: center;
+    }
+</style>
--- wp-content/themes/timber-starter-theme/assets/js/index.js
+++ wp-content/themes/timber-starter-theme/assets/js/index.js
@@ -1,7 +1,20 @@
-import '../scss/main.scss'
-const importAll = () =>  require('./include/importall.js')
+import '../scss/main.scss';
+const importAll = () =>  require('./include/importall.js');
 const images = () => importAll(require.context('../images', false, /\.(png|svg|jpg|jpeg|gif)$/));
-import { createApp } from 'vue'
-import  App from './App.vue';
-const app  = createApp(App);
-app.mount('#app');
+
+import { createApp } from 'vue';
+import header from './components/NavigationMenu.vue';
+import footer from './components/Footer.vue';
+
+
+
+const headerApp = createApp(header);
+headerApp.mount('#nav-main');
+
+
+const footerApp = createApp(footer);
+footerApp.mount('#footer');
+
+
+
+// headerApp.mount('#app');
