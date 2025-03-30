c3d0bbb6191cc1b212db219a575282631f585a49
mauro <maurovargas7725@gmail.com>
2025-03-29 20:11:35 -0600

    Added Direction link to address

--- wp-content/themes/timber-starter-theme/single.php
+++ wp-content/themes/timber-starter-theme/single.php
@@ -14,6 +14,8 @@ $timber_post     = Timber::get_post();
 $context['post'] = $timber_post;
 $context['guides_location'] = get_field('guides_location',$context['post']->ID);
 $context['guides_location']['title'] = $context['post']->title;
+$context['googleDirectionLink'] = "https://www.google.com/maps/dir/?api=1&destination=" . urlencode($context['guides_location']['address']);
+var_dump($context['googleDirectionLink']);
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
