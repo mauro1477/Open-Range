56742c492efc84db718eaeca3f488632f7c36404
mauro <maurovargas7725@gmail.com>
2025-04-08 17:17:45 -0600

    Add condition for vue components index.js

--- wp-content/themes/timber-starter-theme/assets/js/index.js
+++ wp-content/themes/timber-starter-theme/assets/js/index.js
@@ -1,18 +1,19 @@
 import '../scss/main.scss';
-const importAll = () =>  require('./include/importall.js');
-const images = () => importAll(require.context('../images', false, /\.(png|svg|jpg|jpeg|gif)$/));
 import { createApp } from 'vue';
-import SinglePostGoogleMap from './components/SinglePostGoogleMap.vue';
 import header from './components/NavigationMenu.vue';
 import footer from './components/Footer.vue';
 import HomePageAgloliaSearch from './components/HomePageAgloliaSearch.vue'
+import SinglePostGoogleMap from './components/SinglePostGoogleMap.vue';
 
-const HomePageAgloliaSearchApp = createApp(HomePageAgloliaSearch);
-HomePageAgloliaSearchApp.mount('#home-page-algolia-search');
+if (document.getElementById('home-page-algolia-search')) {
+    const HomePageAgloliaSearchApp = createApp(HomePageAgloliaSearch);
+    HomePageAgloliaSearchApp.mount('#home-page-algolia-search');
+} 
 
-const SinglePostGoogleMapApp = createApp(SinglePostGoogleMap);
-SinglePostGoogleMapApp.mount('#google-map-iframe');
-
+if (document.getElementById('google-map-iframe')) {
+    const SinglePostGoogleMapApp = createApp(SinglePostGoogleMap);
+    SinglePostGoogleMapApp.mount('#google-map-iframe');
+} 
 
 const headerApp = createApp(header);
 headerApp.mount('#nav-main');
@@ -20,7 +21,3 @@ headerApp.mount('#nav-main');
 
 const footerApp = createApp(footer);
 footerApp.mount('#footer');
-
-
-
-// headerApp.mount('#app');
