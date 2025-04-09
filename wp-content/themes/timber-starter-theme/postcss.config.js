f44ea1fcf4b2d1dc5c3ce9da972a80f20913ae57
mauro <maurovargas7725@gmail.com>
2025-04-08 21:19:09 -0600

    Minified CSS

--- wp-content/themes/timber-starter-theme/postcss.config.js
+++ wp-content/themes/timber-starter-theme/postcss.config.js
@@ -6,6 +6,14 @@ if(process.env.NODE_ENV == "production"){
             'rucksack-css': {}
         }
     };
+}else if (process.env.NODE_ENV == "staging"){
+    module.exports = {
+        plugins:{
+            autoprefixer: {},
+            cssnano: {},
+            'rucksack-css': {}
+        }
+    };
 }else{
     module.exports = {
         plugins:{
